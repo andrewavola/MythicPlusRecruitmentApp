1db7d5694d427dbd2f34053218f8c553ee0a6bcd
Andrew Avola <andrew.avola@gmail.com>
2023-10-08 15:40:42 -0700

    Added user model, and error handling override
    Todo:
    - UserRoutes and UserController logic

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
-app.use('/api/login', require('./routes/loginRoute'))
 
+app.use('/api/users', require('./routes/userRoutes'))
+
+//Error handler middleware, add in controller function body
+app.use(errorHandler)
 
 
 app.listen(port, () => console.log(`Server started on port ${port}`))

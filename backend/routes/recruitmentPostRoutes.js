4d172d3f75b56499d40b4a79b7d308e08d42ed4d
Andrew Avola <andrew.avola@gmail.com>
2023-10-22 13:29:40 -0700

    Added React Frontend
    Added:
    - React
    - Redux
    - Boilerplate project file format

--- backend/routes/recruitmentPostRoutes.js
+++ backend/routes/recruitmentPostRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const {protect} = require('../middleware/authMiddleware')
 const {createRecPost, deleteRecPost} = require('../controllers/recruitmentPostsController')
 
-router.post('/', createRecPost)
-router.delete('/deleteRecPost', deleteRecPost)
+router.post('/', protect, createRecPost)
+router.delete('/deleteRecPost', protect,  deleteRecPost)
 
 module.exports = router

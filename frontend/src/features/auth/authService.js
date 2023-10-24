e63db0331729f2a493860770c508ed1aed9d5e54
Andrew Avola <andrew.avola@gmail.com>
2023-10-24 13:38:44 -0700

    Login/Register/Logout
    Added:
    - Basic login functionality
    - Basic logout functionality
    - Basic registering functionality
    - Still keeping in mind the need to style the website myself

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -15,8 +15,27 @@ const register  = async(userData) => {
   return response.data
 }
 
+// Logout the user
+const logout = () => {
+  localStorage.removeItem('user')
+}
+
+
+// Login the user
+const login  = async(userData) => {
+  const response = await axios.post(API_URL + 'login', userData)
+
+  if(response.data){
+    localStorage.setItem('user', JSON.stringify(response.data))
+  }
+
+  return response.data
+}
+
 const authService = {
-  register
+  register,
+  logout,
+  login
 }
 
 export default authService

a6f1185225b4dfa815b4341ca5981236d84a4028
Andrew Avola <andrew.avola@gmail.com>
2023-11-25 18:04:49 -0800

    socket.io deployment

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ const server = app.listen(port, () => console.log(`Server started on port ${port
 const io = require('socket.io')(server, {
   // pingTimeout: 60000,
   cors:{
-    origin: "http://localhost:3000"
+    origin: "https://m-recruitment.onrender.com/message"
   }
 })
 

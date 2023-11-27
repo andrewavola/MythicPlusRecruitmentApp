4f171c9e64d77533bd7c698084006c2f88241b65
Andrew Avola <andrew.avola@gmail.com>
2023-11-26 19:03:08 -0800

    test

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,7 @@ app.use(errorHandler)
 
 const io = new Server(server, {
   cors:{
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:5000',
     methods: ["GET", "POST"]
   }
   
